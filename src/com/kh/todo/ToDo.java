94343af9919c076adf5750862bcd64bbfd1ea6c6
Ennakoida <134674257+Ennakoida@users.noreply.github.com>
2023-07-04 17:21:54 +0900

    [Update] 23/07/04 To-Do List
    To-Do List 동작 구현
    
    해야하는 수정사항
    	- 메뉴 입력 오류 시 무한루프
    	- 종료 관련 에러

--- src/com/kh/todo/ToDo.java
+++ src/com/kh/todo/ToDo.java
@@ -1,9 +1,32 @@
 package com.kh.todo;
 
 public class ToDo {
+	private int index;
 	private String list;
 	
 	public ToDo() {
+		index = 0;
 		list = "";
+	}
+	
+	public ToDo(int index, String list) {
+		this.index = index;
+		this.list = list;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	public String getList() {
+		return list;
+	}
+
+	public void setList(String list) {
+		this.list = list;
 	}
 }
--- src/com/kh/todo/ToDoRun.java
+++ src/com/kh/todo/ToDoRun.java
@@ -1,16 +1,17 @@
 package com.kh.todo;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ToDoRun {
 	static Scanner sc = new Scanner(System.in);
+	static ToDo toDoArr[] = new ToDo[100]; // 할일 목록 배열
+	static ToDo doneToDoArr[] = new ToDo[100]; // 완료 목록 배열
+	static int listIndex = 0; // 할일누적용 인덱스
+	static int doneIndex = 0; // 완료 배열 인덱스
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ToDo toDoArr[] = new ToDo[20]; // 할일 목록 배열
-		for(int i = 0; i < toDoArr.length; i++) {
-			toDoArr[i] = new ToDo(); 
-		}
-		
+	
 		finish:
 			while(true) {
 				int choice = printMenu();
@@ -31,6 +32,9 @@ public class ToDoRun {
 					case 5:
 						endProgram();
 						break finish;
+					default:
+						System.out.println("잘못 입력하셨습니다. (1 ~ 5 중 선택)");
+						System.out.println();
 				}
 			}
 	}
@@ -46,20 +50,37 @@ public class ToDoRun {
 		System.out.println("5. 종료하기");
 		System.out.println();
 		System.out.print("메뉴 선택 : ");
-		int choice = sc.nextInt();
-		return choice;
+		int choicenum = 0;
+		try {
+			choicenum = sc.nextInt();
+		} catch (InputMismatchException e) { // 숫자 외 입력 시 예외처리
+			// >>>>>>>>> 이거 무한루프 뜨는데 왜인지 모르겠네............ <<<<<<<<<<<<
+			
+		}
+		
+		return choicenum;
 	}
 
 	private static void insertToDo() {
 		// TODO Auto-generated method stub
 		System.out.println();
 		System.out.println("===== 할 일을 추가해 주세요 =====");
+		System.out.println();
 		System.out.println("초기화면으로 돌아가려면 * 를 입력해주세요.");
-		// 반복문 + 받아오기
-		System.out.println("▶ ");
-		System.out.println("▶ ");
-		System.out.println("▶ ");
-		
+		sc.nextLine();
+		for(int i = listIndex; i < toDoArr.length; i++) {
+			System.out.print("▶ ");
+			String list = sc.nextLine();
+			
+			if(list.equals("*")) {
+				System.out.println("초기화면으로 돌아갑니다.");
+				System.out.println();
+				break;
+			}
+			toDoArr[i] = new ToDo((listIndex + 1), list);
+			
+			listIndex++;
+		}
 	}
 
 	private static void deleteToDo() {
@@ -67,43 +88,74 @@ public class ToDoRun {
 		System.out.println();
 		System.out.println("===== 완료한 할 일을 적어주세요 =====");
 		System.out.println();
+		
+		// 저장된 할 일 목록 출력
 		System.out.println("할 일 목록 : ");
-		// 여기 반복문 처리하기
-		System.out.println("1. ");
-		System.out.println("2. ");
-		System.out.println("3. ");
-		System.out.println("4. ");
-		System.out.println("5. ");
+		for(int i = 0; i < listIndex; i++) { 
+			if(toDoArr[i].getIndex() == 0) { // 삭제되어 값이 없으면 출력하지 않고 넘어감
+				continue;
+			}
+			System.out.println(toDoArr[i].getIndex() + ". " + toDoArr[i].getList());
+		}
+		
+		System.out.println();
 		System.out.println("초기화면으로 돌아가려면 * 를 입력해주세요.");
 		System.out.println();
-		System.out.print("▶ ");
-		int doneList = sc.nextInt(); // 이것도 반복문 처리
-		System.out.print("▶ ");
-		int doneList2 = sc.nextInt(); // 이것도 반복문 처리
 		
-		// * > 초기화면 처리
+		// 완료한 할 일 선택
+		for(int i = 0; i < listIndex; i++) {
+			System.out.print("▶ ");
+			String doneList = sc.next();
+			
+			// 초기화면
+			if(doneList.equals("*")) { 
+				System.out.println();
+				System.out.println("초기화면으로 돌아갑니다.");
+				System.out.println();
+				break;
+			}
+			
+			// 선택한 할 일 삭제(초기화)
+			for(int j = 0; j < listIndex; j++) { 
+				if(toDoArr[j].getIndex() == Integer.parseInt(doneList)) {
+					doneToDoArr[doneIndex] = new ToDo(toDoArr[j].getIndex(), toDoArr[j].getList());
+					doneIndex++;
+					toDoArr[j].setIndex(0);
+					toDoArr[j].setList(null);
+				}			
+			}
+		}
 	}
 
 	private static void showLeftToDo() {
 		// TODO Auto-generated method stub
 		System.out.println();
 		System.out.println("===== 남은 할 일 목록 =====");
+		System.out.println();
+		System.out.println("할 일 목록 : ");
+		for(int i = 0; i < listIndex; i++) { 
+			if(toDoArr[i].getIndex() == 0) { // 삭제되어 값이 없으면 출력하지 않고 넘어감
+				continue;
+			}
+			System.out.println(toDoArr[i].getIndex() + ". " + toDoArr[i].getList());
+		}
+		System.out.println();
 		System.out.println("초기화면으로 돌아가려면 * 를 입력해주세요.");
-		// 반복문 + 받아오기
-		System.out.println("▶ ");
-		System.out.println("▶ ");
-		System.out.println("▶ ");
+		System.out.println();
 	}
 
 	private static void showDoneToDo() {
 		// TODO Auto-generated method stub
 		System.out.println();
 		System.out.println("===== 완료한 할 일 목록 =====");
+		System.out.println();
 		System.out.println("초기화면으로 돌아가려면 * 를 입력해주세요.");
-		// 반복문 + 받아오기
-		System.out.println("▶ ");
-		System.out.println("▶ ");
-		System.out.println("▶ ");
+		System.out.println();
+
+		for(int i = 0; i < doneIndex; i++) {
+			System.out.println("▶ " + doneToDoArr[i].getIndex() + ". " + doneToDoArr[i].getList());			
+		}
+		System.out.println();
 	}
 
 	private static void endProgram() {
@@ -126,7 +178,7 @@ public class ToDoRun {
 				System.out.println("메뉴 선택 화면으로 돌아갑니다.");
 				System.out.println();
 				printMenu();
-				return;
+				break; // 이거 이상함 수정하기
 			} else {
 				System.out.println();
 				System.out.println("잘못입력하셨습니다. 다시 입력해주세요.");
